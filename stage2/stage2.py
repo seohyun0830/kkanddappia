968c8b60651ba0d1a933758ac01fa6711e9e48fa
yunjin <jyunjin0330@gmail.com>
2025-10-27 15:03:28 +0900

    실험실 폭발 및 인벤토리 채우기 1027

--- stage2/stage2.py
+++ stage2/stage2.py
@@ -6,6 +6,7 @@ SCREEN_HEIGHT=800
 
 BLACK=(0,0,0)
 WHITE=(255,255,255)
+RED=(255,0,0)
 
 pygame.init()
 pygame.display.set_caption("KKANDDABBIA!")
@@ -17,13 +18,19 @@ clock=pygame.time.Clock()
 current_path=os.path.dirname(__file__)
 assets_path=os.path.join(current_path, 'kkanddabbia')
 
+font = pygame.font.Font(None, 74) 
+
 start_background_image=pygame.image.load(os.path.join(assets_path, 'outside.jpg'))
 start_background_image=pygame.transform.scale(start_background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-go_to_inside=False
+current_map="outside1" 
 
 background_image=pygame.image.load(os.path.join(assets_path, 'inside.jpg'))
 background_image=pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+second_background_image=pygame.image.load(os.path.join(assets_path, 'outside2.jpg'))
+second_background_image=pygame.transform.scale(second_background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 origin_person_image=pygame.image.load(os.path.join(assets_path, 'person.png')).convert()
 origin_person_image.set_colorkey(WHITE)
@@ -41,161 +48,258 @@ make_image_y=(SCREEN_HEIGHT-make_image.get_height())//2
 person_x=100
 person_y=500
 person_speed=5
-person_direction_right=False
+person_direction_right=True
 
 open_door=False
 
 done=False
 
+game_over=False
+
 CLICK_AREA=pygame.Rect(900,400,210,260)
 OUTSIDE_DOOR_AREA=pygame.Rect(400,400,400,300)
+
+MAKE_BUTTON_X=351
+MAKE_BUTTON_Y=29
+MAKE_BUTTON_W=111
+MAKE_BUTTON_H=111
+
+MAKE_BUTTON_AREA = pygame.Rect(
+    make_image_x + MAKE_BUTTON_X,
+    make_image_y + MAKE_BUTTON_Y,
+    MAKE_BUTTON_W,
+    MAKE_BUTTON_H
+)
+
 
 #################아이템################
 
 ITEM_SIZE=70
 
-inventory=['stone', 'soil', 'wood', 'stick', 'glass', 'window']
+inventory=['fire', 'stone', 'stone', 'wood', 'stick', 'glass', 'window', 'soil', 'stick', 'glass', 'stone', 'wood']
+
+crafting_table=[None]*9
 
 item_images={}
 
-stone_image=pygame.image.load(os.path.join(assets_path, 'stone.png')).convert()
-stone_image.set_colorkey(WHITE)
-item_images['stone']=pygame.transform.scale(stone_image,(ITEM_SIZE, ITEM_SIZE))
+def load_item(name, filename):
+    try:
+        image = pygame.image.load(os.path.join(assets_path, filename))
+        item_images[name] = pygame.transform.scale(image, (ITEM_SIZE, ITEM_SIZE))
+    except pygame.error:
+        print(f"Error loading {filename}. Skipping.")
 
-soil_image=pygame.image.load(os.path.join(assets_path, 'soil.png')).convert()
-soil_image.set_colorkey(WHITE)
-item_images['soil']=pygame.transform.scale(soil_image,(ITEM_SIZE, ITEM_SIZE))
-
-wood_image=pygame.image.load(os.path.join(assets_path, 'wood.png')).convert()
-wood_image.set_colorkey(WHITE)
-item_images['wood']=pygame.transform.scale(wood_image,(ITEM_SIZE, ITEM_SIZE))
-
-stick_image=pygame.image.load(os.path.join(assets_path, 'stick.png')).convert()
-stick_image.set_colorkey(WHITE)
-item_images['stick']=pygame.transform.scale(stick_image,(ITEM_SIZE, ITEM_SIZE))
-
-glass_image=pygame.image.load(os.path.join(assets_path, 'glass.png')).convert()
-glass_image.set_colorkey(WHITE)
-item_images['glass']=pygame.transform.scale(glass_image,(ITEM_SIZE, ITEM_SIZE))
-
-window_image=pygame.image.load(os.path.join(assets_path, 'window.png')).convert()
-window_image.set_colorkey(WHITE)
-item_images['window']=pygame.transform.scale(window_image,(ITEM_SIZE, ITEM_SIZE))
-
-SLOT_POSITIONS=[]
-
-'''
-NUM_COLS=6
-NUM_ROWS=2
-
-X_START_RELATIVE=32
-Y_START_RELATIVE=375
-
-GAP=96
+load_item('fire', 'fire.png')
+load_item('stone', 'stone.png')
+load_item('soil', 'soil.png')
+load_item('wood', 'wood.png')
+load_item('stick', 'stick.png')
+load_item('glass', 'glass.png')
+load_item('window', 'window-piece.png')
 
 
-for row in range(NUM_ROWS):
-    for col in range(NUM_COLS):
-        absolute_x=make_image_x+X_START_RELATIVE+col*GAP
-        absolute_y=make_image_y+Y_START_RELATIVE+row*GAP
+##################제작 조합#################
 
-        SLOT_POSITIONS.append((absolute_x, absolute_y))
+RECIPES=[
+    {
+        'recipe':['stone', 'stone', 'fire', None,None, None, None, None, None],
+        'result':'steel'
+    }
+]
 
-'''
 
-X_POSITIONS=[34, 128, 220, 310, 404, 496]
-Y_POSITIONS=[375,467]
+#################연구실 제작창################
 
-for rel_y in Y_POSITIONS:
-    for rel_x in X_POSITIONS:
+INVENTORY_SLOT_POSITIONS=[]
+INVENTORY_SLOT_RECTS=[]
+
+INV_X_POSITIONS=[34, 128, 220, 310, 404, 496]
+INV_Y_POSITIONS=[375, 467]
+
+for rel_y in INV_Y_POSITIONS:
+    for rel_x in INV_X_POSITIONS:
         absolute_x=make_image_x+rel_x
         absolute_y=make_image_y+rel_y
-        SLOT_POSITIONS.append((absolute_x, absolute_y))
+        INVENTORY_SLOT_POSITIONS.append((absolute_x, absolute_y))
+        
+        INVENTORY_SLOT_RECTS.append(pygame.Rect(absolute_x, absolute_y, ITEM_SIZE, ITEM_SIZE))
 
+
+CRAFT_SLOT_POSITIONS=[]
+CRAFT_SLOT_RECTS=[]
+
+CRAFT_X_POSITIONS = [53, 145, 237] 
+CRAFT_Y_POSITIONS = [62, 154, 246]
+
+for rel_y in CRAFT_Y_POSITIONS:
+    for rel_x in CRAFT_X_POSITIONS:
+        absolute_x=make_image_x+rel_x
+        absolute_y=make_image_y+rel_y
+        CRAFT_SLOT_POSITIONS.append((absolute_x, absolute_y))
+        CRAFT_SLOT_RECTS.append(pygame.Rect(absolute_x, absolute_y, ITEM_SIZE, ITEM_SIZE))
 
 
 #########메인루프#########
+
 
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             done=True
 
-        if event.type==pygame.MOUSEBUTTONDOWN:
+        if event.type==pygame.KEYDOWN and event.key == pygame.K_ESCAPE and game_over:
+            done = True
+
+        if event.type==pygame.MOUSEBUTTONDOWN and not game_over:
             mouse_pos=pygame.mouse.get_pos()
-
-            if go_to_inside==False:
+            
+            if current_map == "outside1":
                 if OUTSIDE_DOOR_AREA.collidepoint(mouse_pos):
-                    go_to_inside=True
+                    current_map="inside"
                     person_x=100
+                    continue
 
-            else:
+            elif current_map == "inside":
                 if CLICK_AREA.collidepoint(mouse_pos):
                     open_door=not open_door
+                    continue
+                
+                if open_door:
+                    
+                    if MAKE_BUTTON_AREA.collidepoint(mouse_pos):
+                        found_recipe=False
+                        
+                        for recipe_data in RECIPES:
+                            if crafting_table==recipe_data['recipe']:
+                                inventory.append(recipe_data['result'])
+                                crafting_table=[None]*9
+                                found_recipe=True
+                                break
+                        
+                        if not found_recipe:
+                            game_over=True
+                            open_door = False
+                            
+                        continue 
+                    
+                    for i in range(len(inventory)):
+                        if i < len(INVENTORY_SLOT_RECTS) and INVENTORY_SLOT_RECTS[i].collidepoint(mouse_pos):
+                            
+                            item_name_clicked = inventory[i] 
+                            
+                            try:
+                                empty_slot_index = crafting_table.index(None) 
+                                crafting_table[empty_slot_index] = item_name_clicked
+                                inventory.pop(i) 
+                                continue
+                            except ValueError:
+                                continue
+                                
+                    for i in range(len(crafting_table)):
+                        if crafting_table[i] is not None and CRAFT_SLOT_RECTS[i].collidepoint(mouse_pos):
+                            inventory.append(crafting_table[i])
+                            crafting_table[i] = None 
+                            continue
 
-                if (not CLICK_AREA.collidepoint(mouse_pos))and (open_door==True):
-                    open_door=not open_door
+                make_window_rect = make_image.get_rect(topleft=(make_image_x, make_image_y))
+                
+                if open_door and not make_window_rect.collidepoint(mouse_pos) and not CLICK_AREA.collidepoint(mouse_pos):
+                    open_door = False
+                        
+            elif current_map == "outside2":
+                 pass
+
+
     
-    keys=pygame.key.get_pressed()
-
-    if go_to_inside==False:
-        if keys[pygame.K_LEFT]:
-            person_x-=person_speed
-            if person_direction_right:
-                person_image=flip_person_image
-                person_direction_right=False
-
-        if keys[pygame.K_RIGHT]:
-            person_x+=person_speed
-            if not person_direction_right:
-                person_image=origin_person_image
-                person_direction_right=True
-
-        if person_x<0:
-            person_x=0
-        elif person_x>SCREEN_WIDTH-person_image.get_width():
-            person_x=SCREEN_WIDTH-person_image.get_width()
-
-        screen.blit(start_background_image, (0,0))
-        screen.blit(person_image, (person_x, person_y))
-
-    else:
-        if keys[pygame.K_LEFT]:
-            person_x-=person_speed
-            if person_direction_right:
-                person_image=flip_person_image
-                person_direction_right=False
-
-        if keys[pygame.K_RIGHT]:
-            person_x+=person_speed
-            if not person_direction_right:
-                person_image=origin_person_image
-                person_direction_right=True
-
-        if person_x<0:
-            person_x=0
-            go_to_inside=False
-        elif person_x>SCREEN_WIDTH-person_image.get_width():
-            person_x=SCREEN_WIDTH-person_image.get_width()
+    if game_over:
+        screen.fill(BLACK)
+        game_over_text=font.render("GAME OVER", True, RED)
+        small_font_for_sub = pygame.font.Font(None, 40)
         
-        screen.blit(background_image, (0,0))
-        screen.blit(person_image, (person_x, person_y))
+        screen.blit(game_over_text, (SCREEN_WIDTH//2 - game_over_text.get_width()//2, SCREEN_HEIGHT//2 - 50))
+    
+    else:
+        keys=pygame.key.get_pressed()
+        
+        if current_map == "outside1":
+            if keys[pygame.K_LEFT]:
+                person_x-=person_speed
+                person_image=flip_person_image
+                person_direction_right=False
+            
+            if keys[pygame.K_RIGHT]:
+                person_x+=person_speed
+                person_image=origin_person_image
+                person_direction_right=True
 
-        if open_door:
-            screen.blit(make_image, (make_image_x, make_image_y))
+            if person_x<0:
+                person_x=0
+            elif person_x > SCREEN_WIDTH - person_image.get_width():
+                current_map = "outside2" 
+                person_x = 5
 
-            have_num=min(len(inventory), len(SLOT_POSITIONS))
+            screen.blit(start_background_image, (0,0))
+            screen.blit(person_image, (person_x, person_y))
+        
+        elif current_map == "inside":
+            if keys[pygame.K_LEFT]:
+                person_x-=person_speed
+                person_image=flip_person_image
+                person_direction_right=False
+            
+            if keys[pygame.K_RIGHT]:
+                person_x+=person_speed
+                person_image=origin_person_image
+                person_direction_right=True
 
-            for i in range(have_num):
-                slot_x, slot_y=SLOT_POSITIONS[i]
+            if person_x<0:
+                current_map = "outside1"
+                person_x = SCREEN_WIDTH - person_image.get_width() - 5 
+            elif person_x>SCREEN_WIDTH-person_image.get_width():
+                person_x=SCREEN_WIDTH-person_image.get_width()
+            
+            screen.blit(background_image, (0,0))
+            screen.blit(person_image, (person_x, person_y))
 
-                item_name=inventory[i]
+            if open_door:
+                screen.blit(make_image, (make_image_x, make_image_y))
 
-                item_draw=item_images.get(item_name)
+                have_num=min(len(inventory), len(INVENTORY_SLOT_POSITIONS))
+                for i in range(have_num):
+                    slot_x, slot_y=INVENTORY_SLOT_POSITIONS[i]
+                    item_name=inventory[i]
+                    item_draw=item_images.get(item_name)
+                    if item_draw:
+                        screen.blit(item_draw, (slot_x, slot_y))
+                        
+                for i in range(len(crafting_table)):
+                    item_name = crafting_table[i]
+                    if item_name is not None:
+                        slot_x, slot_y = CRAFT_SLOT_POSITIONS[i]
+                        item_draw = item_images.get(item_name)
+                        if item_draw:
+                            screen.blit(item_draw, (slot_x, slot_y))
 
-                if item_draw:
-                    screen.blit(item_draw, (slot_x, slot_y))
+        elif current_map == "outside2":
+            if keys[pygame.K_LEFT]:
+                person_x-=person_speed
+                person_image=flip_person_image
+                person_direction_right=False
 
+            if keys[pygame.K_RIGHT]:
+                person_x+=person_speed
+                person_image=origin_person_image
+                person_direction_right=True
+
+            if person_x<0:
+                current_map = "outside1"
+                person_x = SCREEN_WIDTH - person_image.get_width() - 5 
+            elif person_x>SCREEN_WIDTH-person_image.get_width():
+                person_x=SCREEN_WIDTH-person_image.get_width()
+            
+            screen.blit(second_background_image, (0,0))
+            screen.blit(person_image, (person_x, person_y))
+        
 
     pygame.display.flip()
 
