58133ec0aa4fda66b71b6fcbbc5c1bc61beeec82
윤서현 <yunseohyun0830@naver.com>
2025-11-27 16:25:21 +0900

    외부사진 크기 조정

--- start/images.py
+++ start/images.py
@@ -9,6 +9,7 @@ talking = pygame.image.load("start/assets/talking.png")
 grandma = pygame.image.load("start/assets/grandma.png")
 bedroom = pygame.image.load("start/assets/bedroom.png")
 outside = pygame.image.load("start/assets/outside.jpg")
+outside = pygame.transform.smoothscale(outside, (1200, 800))
 
 sfx_click = pygame.mixer.Sound("button/assets/click.wav")
 sfx_type = pygame.mixer.Sound("start/assets/type.mp3")
