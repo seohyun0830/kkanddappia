aa3deb63c6fb9ba2583ec8c8ba3be25a890b266b
suyeon Kim <hangs2712@naver.com>
2025-11-29 21:12:17 +0900

    메인 주석 지움..

--- 3,4stage_test.py
+++ 3,4stage_test.py
@@ -15,7 +15,7 @@ def main():
     pygame.display.set_caption("stage3,4 merge")
 
     assets.load_all()
-    '''
+    
     # -------- Stage 3 --------
     stage3 = Stage3(screen)
     next_stage = stage3.run()
@@ -39,25 +39,6 @@ def main():
                 break
                     
 
-    '''
-    stage4 = Stage4(screen)
-    result = stage4.run()
-
-    # [수정] 들여쓰기 레벨 주의 (if문 밖으로 꺼냈으니 왼쪽으로 당겨야 함)
-    while True:
-        if result == "stage4to3":
-            stage4to3 = Stage4To3(screen)
-            back = stage4to3.run()
-
-            if back == "stage4":
-                # resume() 대신 다시 run()을 하거나 초기화된 상태를 원하면
-                # 상황에 따라 stage4.reset() 같은 게 필요할 수도 있음
-                result = stage4.resume() # 또는 stage4.run()
-            else:
-                break
-        else:
-            # stage4to3이 아니면(클리어하거나 종료했으면) 루프 탈출
-            break
     pygame.quit()
 
 if __name__ == "__main__":
