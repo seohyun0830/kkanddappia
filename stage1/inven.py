8eb0a16e8506223e5cd87f179449bc0befddb184
윤서현 <yunseohyun0830@naver.com>
2025-12-04 18:17:34 +0900

    블록 하나당 5개로 변경

--- stage1/inven.py
+++ stage1/inven.py
@@ -33,9 +33,9 @@ class Cinven:
         for item_id in self.invenList:
             idx = item_id - 1 # 아이템 인덱스 (0~4)
 
-            if temp_cnt[idx] >= 10:
-                self.blockCount.append(10) # 10개 꽉 채움
-                temp_cnt[idx] -= 10
+            if temp_cnt[idx] >= 5:
+                self.blockCount.append(5) # 10개 꽉 채움
+                temp_cnt[idx] -= 5
             else:
                 self.blockCount.append(temp_cnt[idx]) # 남은 거 다 넣음
                 temp_cnt[idx] = 0
@@ -146,6 +146,6 @@ class Cinven:
                 self.invenCnt[itemMap[blockY][blockX] - 1] += 1
             else:
                 self.invenList.append(itemMap[blockY][blockX])
-                self.invenCnt[itemMap[blockY][blockX] - 1] += 10
+                self.invenCnt[itemMap[blockY][blockX] - 1] += 5
             self.invenList.sort()
             itemMap[blockY][blockX] = 0 # 맵에서 아이템 제거
