8134d616208207d406f65be02296e7d19b249625
suyeon Kim <hangs2712@naver.com>
2025-12-09 14:04:31 +0900

    게이지 원상복구

--- stage4/stage4.py
+++ stage4/stage4.py
@@ -127,7 +127,7 @@ class Stage4:
             self.game_elapsed_time = game_elapsed_time
 
             # 연료 부족 → stage4to3 이동
-            if fuel_manager.fuel < 60 and self.fule_stage4to3==False:  # 테스트용 기준
+            if fuel_manager.fuel < 20 and self.fule_stage4to3==False:  # 테스트용 기준
 
                 pygame.mixer.music.pause()
                 self.screen.blit(self.fuel_failure_img, (0, 0))
@@ -142,7 +142,7 @@ class Stage4:
                 self.fule_stage4to3=True
                 return "stage4to3"
             ####연료 부족
-            if fuel_manager.fuel < 60 and self.fule_stage4to3==True:    #이미 3갔다왔으면
+            if fuel_manager.fuel < 20 and self.fule_stage4to3==True:    #이미 3갔다왔으면
                 sounds_to_play={'fuel_empty':self.fuel_empty_sound,
                                     'siren':self.siren_sound}
                 crash_ani.fuel_empty_ani(self.screen,self.fuel_empty_img,sounds_to_play)
